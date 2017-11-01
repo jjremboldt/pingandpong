14176e93da5cbbe1a8f2d9f5b460a7ec09f759e5
John Remboldt <jjremboldt@icloud.com>
2017-11-01 16:46:06 -0700

    initial files

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+
+// Frontend logic
+$(document).ready(function() {
+  $("form#yourNumber").submit(function(event) {
+    event.preventDefault();
+  $("ul#list").empty();
+
+    var input = parseInt($("input#yourNumber").val());
+    var result = pingPong(input);
+    result.forEach(function(result) {
+      $("ul#list").append("<li>" + result + "</li>");
+    });
+  });
+});
+
+// Backend logic
+var pingPong = function(input) {
+  var resultPongs = [];
+  for (var index = 1; index <= input; index++) {
+    if (index % 15 === 0) {
+      resultPongs.push("pingpong");
+    } else if (index % 5 === 0) {
+      resultPongs.push("pong");
+    } else if (index % 3 === 0) {
+      resultPongs.push("ping");
+    } else {
+      resultPongs.push(index);
+    }
+  }
+  return resultPongs;
+
+  (function() {
+    location.reload();
+  });
+};
